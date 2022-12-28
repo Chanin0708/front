7c323588fc50c732cdd0cfeae4b2232b6f70c403
chanin <chanin.ph@intelligist.co.th>
2022-12-28 15:59:02 +0700

    update create

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,63 +1,55 @@
 <template>
   <div>
-    <b-card style="width: 1400px; height: auto" class="mx-auto">
-      <div>
-        <!-- <b-button id="show-btn"  variant="success" class="mb-3 " style="justify-content: flex-end;" @click="$bvModal.show('bv-modal-example')>เพิ่มผลไม้ </b-button> -->
-        <b-button id="show-btn" @click="$bvModal.show('bv-modal-example')" variant="success" class="mb-3">เพิ่มผลไม้</b-button>
+    <div>
+      <b-card style="width: 1400px; height: auto" class="mx-auto">
+        <div class="row">
 
-        <!-- <b-modal id="show-btn" title="Add Values and Image" hide-footer>
-    <b-form @submit.stop.prevent="onSubmit">
-      <b-form-group label="Name : " label-for="value1">
-        <b-form-input v-model="value1" :state="validation"></b-form-input>
-      </b-form-group>
-      <b-form-group label="Photo" label-for="image">
-        <b-form-file v-model="image" accept="image/*"></b-form-file>
-      </b-form-group>
-      <b-button type="submit" variant="primary" >Add</b-button>
-    </b-form>
-  </b-modal> -->
-  <b-modal id="bv-modal-example" hide-footer style="width: 500px; height: auto">
-    <template #modal-title>
-      Create
-    </template>
-    <div class="d-block">
-      <b-form @submit.stop.prevent="onSubmit">
-      <b-form-group label="Name : " label-for="value1">
-        <b-form-input v-model="value1" :state="validation"></b-form-input>
-      </b-form-group>
-      <b-form-group label="Photo" label-for="image">
-        <b-form-file v-model="image" accept="image/*"></b-form-file>
-      </b-form-group>
-    </b-form>
-    </div>
-    <b-button class="mt-3" block type="submit"  @click="save" variant="primary" >save</b-button>
+          <div class="col-12 d-flex justify-content-end">
+            <b-button id="show-btn" @click="$bvModal.show('bv-modal-example')" variant="success"
+              class="mb-3 mr-3">เพิ่มผลไม้</b-button>
+          </div>
+          <div>
+            <b-modal id="bv-modal-example" hide-footer style="width: 500px; height: auto">
+              <template #modal-title>
+                Create
+              </template>
+              <div class="d-block">
+                <b-form @submit.stop.prevent="onSubmit">
+                  <b-form-group label="Name : " label-for="name">
+                    <b-form-input v-model="name" :state="validation"></b-form-input>
+                  </b-form-group>
+                  <b-form-group label="Photo" label-for="photo">
+                    <b-form-file v-model="photo" accept="image/*"></b-form-file>
+                  </b-form-group>
+                  <div class="d-flex justify-content-end">
+                    <b-button class="mt-3 mx-2" block type="submit" variant="primary">save</b-button>
+                    <b-button class="mt-3 mx-2" block @click="$bvModal.hide('bv-modal-example')"
+                      variant="danger">cancel</b-button>
+                  </div>
+                </b-form>
+              </div>
+            </b-modal>
+          </div>
 
-    <b-button class="mt-3 ml-3" block type="submit" @click="$bvModal.hide('bv-modal-example')" variant="primary" >cancel</b-button>
-  </b-modal>
-
-
-        <div>
-          <b-form-input
-            v-model="text"
-            placeholder="Search here"
-            style="width: 200px; height: auto"
-            class="mx-auto"
-          ></b-form-input>
-          <!-- <div class="mt-2">Value: {{ text }}</div> -->
+          <div class="col-12 d-flex justify-content-end">
+            <b-form-input v-model="search" placeholder="Search for names" @input="searchName"
+              style="width: 200px; height: auto"></b-form-input>
+          </div>
         </div>
-      </div>
-    </b-card>
+      </b-card>
+    </div>
+
     <b-card style="width: 1400px; height: auto" class="mx-auto">
       <div v-if="fruitlist.length > 0">
         <div class="pt-3 mx-auto">
           <b-table striped hover :items="fruitlist" :fields="['name', 'photo']">
-    <template v-slot:cell(name)="data">
-      {{ data.item.name }}
-    </template>
-    <template v-slot:cell(photo)="data">
-      <img :src="data.item.filePath" alt="fruit photo" />
-    </template>
-  </b-table>
+            <template v-slot:cell(name)="data">
+              {{ data.item.name }}
+            </template>
+            <template v-slot:cell(photo)="data">
+              <img :src="data.item.filePath" alt="fruit photo" />
+            </template>
+          </b-table>
         </div>
       </div>
     </b-card>
@@ -65,13 +57,15 @@
   </div>
 </template>
 <script>
-
+import axios from 'axios';
 
 export default {
 
   data() {
     return {
-      value1: '',
+      search: '',
+      data: [],
+      name: '',
       image: null,
       fruitlist: [],
       fields: [
@@ -82,39 +76,93 @@ export default {
       text: "",
     };
   },
-  mounted(){
+  mounted() {
     this.getfruit();
+    this.save();
+    this.searchName();
+  },
+  computed: {
+    filteredData() {
+      return this.data.filter((item) => item.name.toLowerCase().includes(this.search.toLowerCase()));
+    }
   },
   methods: {
+    onSubmit() {
+      const formData = new FormData();
+      formData.append('name', this.name);
+      formData.append('photo', this.photo);
+
+      axios
+        .post("http://localhost:3000" + "/upload", formData)
+        .then((response) => {
+          console.log(response.data);
+          // Close the modal
+          this.$bvModal.hide('bv-modal-example');
+          // Display a notification based on the response message
+          if (response.data.message === 'Please provide a value, and photo.') {
+            this.$bvToast.toast('Error: ' + response.data.message, {
+              title: 'Error',
+              variant: 'danger',
+              autoHideDelay: 0,
+              toaster: 'b-toaster-top-center'
+            });
+          } else if (response.data.message === 'Value and photo successfully added.') {
+            this.$bvToast.toast(response.data.message, {
+              title: 'Success',
+              variant: 'success',
+              autoHideDelay: 0,
+              toaster: 'b-toaster-top-center'
+            });
+            // Refresh the page
+            location.reload();
+          }
+        })
+        .catch((error) => {
+          console.log(error);
+          // Check the status code
+          if (error.response.status === 400) {
+            // Display a notification that stays on the screen until the user closes it
+            this.$bvToast.toast('Error: ' + error.response.data.message, {
+              title: 'Error',
+              variant: 'danger',
+              autoHideDelay: 0,
+              toaster: 'b-toaster-top-center'
+            });
+          } else {
+            // Display a notification with a default duration
+            this.$bvToast.toast('Error: ' + error, {
+              title: 'Error',
+              variant: 'danger',
+              autoHideDelay: 3000,
+              toaster: 'b-toaster-top-center'
+            });
+          }
+        });
+    },
     getfruit() {
       console.log("aaaaa")
-      this.axios.get("http://localhost:3000" + "/getdatafruit",{
+      this.axios.get("http://localhost:3000" + "/getdatafruit", {
         headers: {
           'Access-Control-Allow-Origin': '*',
         }
-      }).then((response) =>{
+      }).then((response) => {
         console.log("fdsfsdf")
         console.log(response.data.data)
-        this.fruitlist=response.data.data;
+        this.fruitlist = response.data.data;
         console.log(this.fruitlist)
       })
     }
-
   },
-  save() {
-    console.log(name)
-      this.axios
-        .post("http://localhost:3000" + "/upload", {
-          name: this.category_name,
-          author: "",
-          photo: this.photo
-          
-        }).then((response) =>{
-      //   console.log("fdsfsdf")
-        console.log(response.message)
-      //   this.fruitlist=response.data.data;
-      //   console.log(this.fruitlist)
+  searchName() {
+    axios
+      .get("http://localhost:3000" + "/getdatafruit" + this.search)
+      .then((response) => {
+        this.fruitlist = response.data;
       })
-    },
+      .catch((error) => {
+        console.log(error);
+      });
+  }
+
 };
 </script>
